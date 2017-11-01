21adbd58d702fb36305d9537902eaeea1e1fa66a
EDDragonWolf <eddragonwolf@ukr.net>
2017-11-01 03:10:55 -0700

    Added install_dependencies shell script

---
+++ install_dependencies.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y \
+    build-essential \
+    cmake \
+    pkg-config \
+    libboost-all-dev \
+    libssl-dev \
+    libunbound-dev \
+    libminiupnpc-dev \
+    libunwind8-dev \
+    liblzma-dev \
+    libldns-dev \
+    libexpat1-dev \
+    doxygen \
+    graphviz
+
+sudo apt-get install libgtest-dev && cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/
