78a5d0c075e6943e3fcc00ebcb5e5b1b42106966
Ilya Kitaev <mbg033@gmail.com>
2017-12-26 20:10:36 +0300

    Revert "x100 and network restart via increased block version"

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -533,7 +533,6 @@ namespace cryptonote
     switch (decimal_point)
     {
       case 12:
-      case 10:
       case 9:
       case 6:
       case 3:
@@ -552,7 +551,6 @@ namespace cryptonote
   //---------------------------------------------------------------
   std::string get_unit(unsigned int decimal_point)
   {
-    return "graft";
     if (decimal_point == (unsigned int)-1)
       decimal_point = default_decimal_point;
     switch (std::atomic_load(&default_decimal_point))
