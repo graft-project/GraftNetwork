e4a4176212ec213107649b9e8f291e55462ab00f
Ilya Kitaev <mbg033@gmail.com>
2018-04-04 21:07:54 +0300

    use new PoW only for v8 hardfork

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -747,7 +747,7 @@ namespace cryptonote
   {
 
     blobdata bd = get_block_hashing_blob(b);
-    const int cn_variant = b.major_version >= 7 ? b.major_version - 6 : 0;
+    const int cn_variant = b.major_version >= 8 ? b.major_version - 7 : 0;
     crypto::cn_slow_hash(bd.data(), bd.size(), res, cn_variant);
     return true;
   }
