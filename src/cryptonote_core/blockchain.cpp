743bc60ef68dc6c0ce209b4f3a12ad757ead0db8
Ilya Kitaev <mbg033@gmail.com>
2018-04-05 18:53:54 +0300

    private testnet hf8 block adjusted

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -137,7 +137,7 @@ static const struct {
   // GRAFT: start hardfork v7 from 1st block
   { 7, 1, 0, 1501709789 },
   // GRAFT: private testnet hardfork v8 from block 57010
-  { 8, 55760, 0, 1522838800 },
+  { 8, 55732, 0, 1522838800 },
 };
 // static const uint64_t testnet_hard_fork_version_1_till = 624633;
 static const uint64_t testnet_hard_fork_version_1_till = 1;
