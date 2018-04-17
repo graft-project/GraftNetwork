1c2bdc31c1e267318da4e647d38bd4d270646864
Ilya Kitaev <mbg033@gmail.com>
2018-04-17 20:03:14 +0300

    HF8 block updated

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -107,9 +107,9 @@ static const struct {
   { 6, 1400000, 0, 1503046577 },
   */
   // GRAFT: start hardfork v7 from 1st block
-  { 7, 1, 0, 1503046577 },
+  { 7, 1, 0, 1503046577 } ,
   // GRAFT: updated v8 hf block
-  { 8, 64408, 0, 1523570400 },
+  { 8, 64445, 0, 1523570400 },
 };
 // static const uint64_t mainnet_hard_fork_version_1_till = 1009826;
 static const uint64_t mainnet_hard_fork_version_1_till = 1;
