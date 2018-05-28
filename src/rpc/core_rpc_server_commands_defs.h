65d86eea87576afe20ac38f6a77cc65a783b9dce
Ilya Kitaev <mbg033@gmail.com>
2018-05-28 14:50:28 +0300

    add: callback_url param rta_authorize_tx rpc

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -1720,12 +1720,14 @@ namespace cryptonote
     struct request
     {
       // TODO: we don't care about data here, just testing flow for now
+      std::string callback_url;
       uint64_t timestamp;
       std::string src_address;
       std::string dst_address;
       std::string tx_id;
       u_int64_t amount;
       BEGIN_KV_SERIALIZE_MAP()
+        KV_SERIALIZE(callback_url)
         KV_SERIALIZE(timestamp)
         KV_SERIALIZE(src_address)
         KV_SERIALIZE(dst_address)
