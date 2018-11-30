399aa70163f58c8fe85ec89a4450f555c3d4dabf
Leny Kholodov <leny.kholodov@graft.network>
2018-11-30 18:00:10 +0200

    Debug format fixes

--- external/easylogging++/easylogging++.h
+++ external/easylogging++/easylogging++.h
@@ -1052,8 +1052,8 @@ typedef base::threading::internal::NoScopedLock<base::threading::Mutex> ScopedLo
 /// @brief Base of thread safe class, this class is inheritable-only
 class ThreadSafe {
  public:
-  virtual inline void acquireLock(void) ELPP_FINAL { printf("acquireLock: %p, thisThreadId=%d, ownerThreadId=%d\n", m_mutex.native_handle(), int(std::hash<std::thread::id>()(std::this_thread::get_id())), m_thread_id); fflush(stdout); m_mutex.lock(); m_thread_id = std::hash<std::thread::id>()(std::this_thread::get_id()); }
-  virtual inline void releaseLock(void) ELPP_FINAL { printf("releaseLock: %p\n, thisThreadId=%d, ownerThreadId=%d", m_mutex.native_handle(), int(std::hash<std::thread::id>()(std::this_thread::get_id())), m_thread_id); fflush(stdout); m_thread_id = 0; m_mutex.unlock(); }
+  virtual inline void acquireLock(void) ELPP_FINAL { printf("tryAcquireLock: %p, thisThreadId=%u, ownerThreadId=%u\n", m_mutex.native_handle(), int(std::hash<std::thread::id>()(std::this_thread::get_id())), m_thread_id); fflush(stdout); m_mutex.lock(); m_thread_id = std::hash<std::thread::id>()(std::this_thread::get_id()); printf("acquiredLock!: %p, thisThreadId=%u, ownerThreadId=%u\n", m_mutex.native_handle(), int(std::hash<std::thread::id>()(std::this_thread::get_id())), m_thread_id); fflush(stdout);}
+  virtual inline void releaseLock(void) ELPP_FINAL { printf("releaseLock: %p, thisThreadId=%u, ownerThreadId=%u\n", m_mutex.native_handle(), int(std::hash<std::thread::id>()(std::this_thread::get_id())), m_thread_id); fflush(stdout); m_thread_id = 0; m_mutex.unlock(); }
   virtual inline base::threading::Mutex& lock(void) ELPP_FINAL { return m_mutex; }
  protected:
   ThreadSafe(void) {}
