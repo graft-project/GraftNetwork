ff63d9102df0d9e9fc7c0192cf0013e5ad529eea
Ilya Kitaev <mbg033@gmail.com>
2019-03-13 22:45:17 +0300

    HF13 for private testnet

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -151,7 +151,9 @@ static const struct {
   // hf 11 - Monero V8/CN variant 2 PoW, 2018-10-24
   { 11, 194130, 0, 1540400400 },
   // hf 12 - Graft CryptoNight Reverse Waltz PoW, ~2019-03-05T05:00:00+00
-  { 12, 286500, 0, 1551762000 }
+  { 12, 286500, 0, 1551762000 },
+  // hf 13 - RTA transactions, RTA mining
+  { 13, 287171, 0, 1552510800 }
 };
 // static const uint64_t testnet_hard_fork_version_1_till = 624633;
 static const uint64_t testnet_hard_fork_version_1_till = 1;
