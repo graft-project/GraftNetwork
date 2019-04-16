0a4554a841b3d852a64f7fba12b591562e7d3afe
EDDragonWolf <eddragonwolf@ukr.net>
2019-04-16 10:27:53 -0700

    Fixed HF versions for Monero v0.13 features

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -139,12 +139,13 @@
 
 #define THREAD_STACK_SIZE                       5 * 1024 * 1024
 
+#define HF_VERSION_MONERO_13                    14
 #define HF_VERSION_DYNAMIC_FEE                  4
-#define HF_VERSION_MIN_MIXIN_4                  13
+#define HF_VERSION_MIN_MIXIN_4                  6
 #define HF_VERSION_MIN_MIXIN_6                  7
-#define HF_VERSION_MIN_MIXIN_10                 13
+#define HF_VERSION_MIN_MIXIN_10                 HF_VERSION_MONERO_13
 #define HF_VERSION_ENFORCE_RCT                  6
-#define HF_VERSION_PER_BYTE_FEE                 13
+#define HF_VERSION_PER_BYTE_FEE                 HF_VERSION_MONERO_13
 
 #define PER_KB_FEE_QUANTIZATION_DECIMALS        8
 
