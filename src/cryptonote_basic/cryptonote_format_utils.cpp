78d049808ce893d6d020b529d2dfdc537bf9c20b
Alexander Suprunenko <alexander.suprunenko@graft.network>
2019-05-06 02:21:01 +0300

    Disqualification transaction support added.

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -1325,4 +1325,38 @@ namespace cryptonote
       return false;
     return ::serialization::parse_binary(rta_signatures_data.data, rta_signatures);
   }
+
+  bool graft_is_disqualification(const transaction &tx)
+  {
+    if(tx.version != 123) return false;
+    std::vector<tx_extra_field> tx_extra_fields;
+    parse_tx_extra(tx.extra, tx_extra_fields);
+    tx_extra_graft_disqualification disq;
+    if(!find_tx_extra_field_by_type(tx_extra_fields, disq))
+      return false;
+    return true;
+  }
+
+  bool graft_check_disqualification(const transaction &tx)
+  {
+    if(tx.version != 123) return false;
+    if(!tx.vin.empty() || !tx.vout.empty() || tx.rct_signatures.txnFee !=0) return false;
+    std::vector<tx_extra_field> tx_extra_fields;
+    parse_tx_extra(tx.extra, tx_extra_fields);
+    tx_extra_graft_disqualification disq;
+    if(!find_tx_extra_field_by_type(tx_extra_fields, disq))
+      return false;
+    {//check signs
+      std::string item_str;
+      ::serialization::dump_binary(disq.item, item_str);
+      crypto::hash hash;
+      crypto::cn_fast_hash(item_str.data(), item_str.size(), hash);
+      for(auto& si : disq.signers)
+      {
+        if(!crypto::check_signature(hash, si.signer_id, si.sign)) return false;
+      }
+    }
+    return true;
+  }
+
 }
