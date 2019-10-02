70dd1183752d4b39bdd2110c324ace243bf34f50
Jason Rhinelander <jason@imaginary.ca>
2019-10-02 16:09:32 -0300

    Add quorum conn index iterator for incoming conns
    Quorumnet channels are bidirectional, which means we want to be able to
    broadcast info to nodes that connect to us (if they have done so).  This
    paves the way by providing an iterator through incoming connection
    quorum members.

--- src/quorumnet/conn_matrix.h
+++ src/quorumnet/conn_matrix.h
@@ -39,12 +39,16 @@ namespace quorumnet {
 class quorum_conn_iterator {
 private:
     friend class quorum_outgoing_conns;
-    const bool *flags;
+    friend class quorum_incoming_conns;
+    const bool * const flags;
     int i;
     const int N;
+    const int step;
 
-    quorum_conn_iterator(const bool *flags_, int N_) : flags{flags_}, i{0}, N{N_} {
-        if (!flags[i] && i < N) ++*this;
+    quorum_conn_iterator(const bool *flags_, int N_, bool outgoing_ = true) : flags{flags_}, i{0}, N{N_}, step{outgoing_ ? 1 : N} {
+        if (flags == nullptr && N != 0)
+            throw std::domain_error("Invalid/unsupported quorum size (" + std::to_string(N) + ")");
+        if (!flags[0] && N > 0) ++*this;
     }
 
 public:
@@ -54,7 +58,7 @@ public:
     using pointer_type = const int *;
     using reference = const int &;
 
-    constexpr quorum_conn_iterator() : flags{nullptr}, i{0}, N{0} {}
+    constexpr quorum_conn_iterator() : flags{nullptr}, i{0}, N{0}, step{1} {}
 
     reference operator*() const { return i; }
 
@@ -65,7 +69,7 @@ public:
     bool operator!=(const quorum_conn_iterator &it) const { return !(*this == it); }
 
     quorum_conn_iterator &operator++() {
-        do { ++i; } while (i < N && !flags[i]);
+        do { ++i; } while (i < N && !flags[i*step]);
         return *this;
     }
 
@@ -287,36 +291,51 @@ template<> constexpr std::array<bool, 20*20> quorum_conn_matrix<20>{{
 /// Returns the maximum possible outgoing connections for a single quorum of any supported size.
 constexpr int max_outgoing_conns() { return 5; }
 
-/// Iterable class that lets you iterate through peer indices that should have an outgoing
-/// connection
-class quorum_outgoing_conns {
-private:
-    quorum_conn_iterator begin_;
+constexpr const bool *get_matrix(int N) {
+    switch (N) {
+        case 7: return &quorum_conn_matrix<7>[0];
+        case 8: return &quorum_conn_matrix<8>[0];
+        case 9: return &quorum_conn_matrix<9>[0];
+        case 10: return &quorum_conn_matrix<10>[0];
+        case 13: return &quorum_conn_matrix<13>[0];
+        case 14: return &quorum_conn_matrix<14>[0];
+        case 15: return &quorum_conn_matrix<15>[0];
+        case 16: return &quorum_conn_matrix<16>[0];
+        case 17: return &quorum_conn_matrix<17>[0];
+        case 18: return &quorum_conn_matrix<18>[0];
+        case 19: return &quorum_conn_matrix<19>[0];
+        case 20: return &quorum_conn_matrix<20>[0];
+        default: return nullptr;
+    };
+}
 
-    static const bool *get_matrix(int N) {
-        switch (N) {
-            case 7: return &quorum_conn_matrix<7>[0];
-            case 8: return &quorum_conn_matrix<8>[0];
-            case 9: return &quorum_conn_matrix<9>[0];
-            case 10: return &quorum_conn_matrix<10>[0];
-            case 13: return &quorum_conn_matrix<13>[0];
-            case 14: return &quorum_conn_matrix<14>[0];
-            case 15: return &quorum_conn_matrix<15>[0];
-            case 16: return &quorum_conn_matrix<16>[0];
-            case 17: return &quorum_conn_matrix<17>[0];
-            case 18: return &quorum_conn_matrix<18>[0];
-            case 19: return &quorum_conn_matrix<19>[0];
-            case 20: return &quorum_conn_matrix<20>[0];
-            default: throw std::domain_error("Invalid/unsupported quorum size (" + std::to_string(N) + ")");
-        };
-    }
+
+/// Iterable class that lets you iterate through peer indices that should have an outgoing
+/// connection.  (Internally this is traversing all indices of the connection matrix with a true
+/// value along the given row).
+class quorum_outgoing_conns {
+    quorum_conn_iterator begin_;
 
 public:
     /// Constructs an iterable object iterating over the peers of node `i` in a quorum of size `N`
     quorum_outgoing_conns(int i, int N) : begin_{get_matrix(N) + i*N, N} {}
 
     using iterator = quorum_conn_iterator;
+    iterator begin() const { return begin_; }
+    constexpr iterator end() const { return {}; }
+};
 
+/// Iterable class that lets you iterator through peer indices of expected inbound connections for
+/// the quorum.  (Internally this is traversing all indices of the connection matrix with a true
+/// value along the given column).
+class quorum_incoming_conns {
+    quorum_conn_iterator begin_;
+
+public:
+    /// Constructs an iterable object iterating over the peers of node `i` in a quorum of size `N`
+    quorum_incoming_conns(int i, int N) : begin_{get_matrix(N) + i, N, false} {}
+
+    using iterator = quorum_conn_iterator;
     iterator begin() const { return begin_; }
     constexpr iterator end() const { return {}; }
 };
