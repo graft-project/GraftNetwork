a2015a1a1c1bc40feb4646fb2347bf853dffb06f
Jason Rhinelander <jason@imaginary.ca>
2019-10-08 22:36:32 -0300

    Increase maximum uptime credit to 48h (#882)
    You'll now hit this maxmimum if you've been up for ~60d.  Note that this
    will not fully apply until the whole network has upgraded to v5; before
    the fork height non-upgraded SNs voting for you will still only award
    max 24h credit.

--- src/cryptonote_core/service_node_rules.h
+++ src/cryptonote_core/service_node_rules.h
@@ -22,7 +22,7 @@ namespace service_nodes {
   // deregistration transaction to the network, starting a 30-day deregistration count down.
   constexpr int64_t DECOMMISSION_CREDIT_PER_DAY = BLOCKS_EXPECTED_IN_HOURS(24) / 30;
   constexpr int64_t DECOMMISSION_INITIAL_CREDIT = BLOCKS_EXPECTED_IN_HOURS(2);
-  constexpr int64_t DECOMMISSION_MAX_CREDIT     = BLOCKS_EXPECTED_IN_HOURS(24);
+  constexpr int64_t DECOMMISSION_MAX_CREDIT     = BLOCKS_EXPECTED_IN_HOURS(48);
   constexpr int64_t DECOMMISSION_MINIMUM        = BLOCKS_EXPECTED_IN_HOURS(2);
 
   static_assert(DECOMMISSION_INITIAL_CREDIT <= DECOMMISSION_MAX_CREDIT, "Initial registration decommission credit cannot be larger than the maximum decommission credit");
