951a93f1fa1c12fc1cc9459195fd1871bdda9365
Doyle <doylet@protonmail.com>
2019-10-10 11:43:08 +1100

    dockerfile: Build Sodium before ZMQ for --with-libsodium for symbol decl conflicts
    Make collect script more sane, don't go backwards in directory

--- utils/build_scripts/collect_from_docker_container.sh
+++ utils/build_scripts/collect_from_docker_container.sh
@@ -1,4 +1,4 @@
-set -ex && mkdir -p ../../build/release/bin
+set -ex && mkdir -p build/release/bin
 set -ex && docker create --name loki-daemon-container loki-daemon-image
-set -ex && docker cp loki-daemon-container:/usr/local/bin/ ../../build/
+set -ex && docker cp loki-daemon-container:/usr/local/bin/ build/release/
 set -ex && docker rm loki-daemon-container
