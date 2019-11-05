48fe82e812b1ef9865777060af46af7a5dc5af71
Jason Rhinelander <jason@imaginary.ca>
2019-11-05 14:40:32 -0400

    LockedTXN fixes/simplifications
    The default copy and move constructors would be broken and dangerous;
    delete the copy ctor and provide a usable move ctor.  Also explicitly
    delete copy and move assignment (they are implicitly deleted already but
    making this explicit signals intent).
    
    We can also save a bit of indirection by storing the BlockchainDB
    reference directly rather than the Blockchain (which isn't needed except
    to get the db).
    
    Also remove the `m_active` variable: the two bools here are doing
    nothing that a single m_batch bool can't do.

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -100,17 +100,20 @@ namespace cryptonote
     // the whole prepare/handle/cleanup incoming block sequence.
     class LockedTXN {
     public:
-      LockedTXN(Blockchain &b): m_blockchain(b), m_batch(false), m_active(false) {
-        m_batch = m_blockchain.get_db().batch_start();
-        m_active = true;
+      LockedTXN(Blockchain &b): m_db{b.get_db()} {
+        m_batch = m_db.batch_start();
       }
-      void commit() { try { if (m_batch && m_active) { m_blockchain.get_db().batch_stop(); m_active = false; } } catch (const std::exception &e) { MWARNING("LockedTXN::commit filtering exception: " << e.what()); } }
-      void abort() { try { if (m_batch && m_active) { m_blockchain.get_db().batch_abort(); m_active = false; } } catch (const std::exception &e) { MWARNING("LockedTXN::abort filtering exception: " << e.what()); } }
+      LockedTXN(const LockedTXN &) = delete;
+      LockedTXN &operator=(const LockedTXN &) = delete;
+      LockedTXN(LockedTXN &&o) : m_db{o.m_db}, m_batch{o.m_batch} { o.m_batch = false; }
+      LockedTXN &operator=(LockedTXN &&) = delete;
+
+      void commit() { try { if (m_batch) { m_db.batch_stop(); m_batch = false; } } catch (const std::exception &e) { MWARNING("LockedTXN::commit filtering exception: " << e.what()); } }
+      void abort() { try { if (m_batch) { m_db.batch_abort(); m_batch = false; } } catch (const std::exception &e) { MWARNING("LockedTXN::abort filtering exception: " << e.what()); } }
       ~LockedTXN() { this->abort(); }
     private:
-      Blockchain &m_blockchain;
+      BlockchainDB &m_db;
       bool m_batch;
-      bool m_active;
     };
   }
   //---------------------------------------------------------------------------------
