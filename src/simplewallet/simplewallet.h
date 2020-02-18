5db01be8aa3f3dd8db6cd49564b9f8310ff9a876
Jason Rhinelander <jason@imaginary.ca>
2020-02-18 17:53:40 -0400

    Move hard-coded Monero donation address
    No need for this to be in the wallet code when we don't use it (it is
    used in one test case, but that is now simply hard-coded there instead).

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -52,8 +52,6 @@
 
 #undef LOKI_DEFAULT_LOG_CATEGORY
 #define LOKI_DEFAULT_LOG_CATEGORY "wallet.simplewallet"
-// Hardcode Monero's donation address (see #1447)
-constexpr const char MONERO_DONATION_ADDR[] = "44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A";
 
 /*!
  * \namespace cryptonote
