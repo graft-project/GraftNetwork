b3980dff3797bc0ff0666109f613e66e11635bf1
Doyle <doylet@protonmail.com>
2020-04-09 12:17:39 +1000

    s/varialble/variable typos have a development cost as well

--- contrib/epee/include/serialization/keyvalue_serialization.h
+++ contrib/epee/include/serialization/keyvalue_serialization.h
@@ -70,8 +70,8 @@ public: \
 #define KV_SERIALIZE_VALUE(variable) \
   epee::serialization::selector<is_store>::serialize(variable, stg, hparent_section, #variable);
 
-#define KV_SERIALIZE_N(varialble, val_name) \
-  epee::serialization::selector<is_store>::serialize(this_ref.varialble, stg, hparent_section, val_name);
+#define KV_SERIALIZE_N(variable, val_name) \
+  epee::serialization::selector<is_store>::serialize(this_ref.variable, stg, hparent_section, val_name);
 
   template<typename T> inline void serialize_default(const T &t, T v) { }
   template<typename T, typename S> inline void serialize_default(T &t, S &&v) { t = std::forward<S>(v); }
@@ -91,31 +91,31 @@ do { \
   } \
 } while (0);
 
-#define KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(varialble, val_name) \
-  epee::serialization::selector<is_store>::serialize_t_val_as_blob(this_ref.varialble, stg, hparent_section, val_name); 
+#define KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(variable, val_name) \
+  epee::serialization::selector<is_store>::serialize_t_val_as_blob(this_ref.variable, stg, hparent_section, val_name); 
 
-#define KV_SERIALIZE_VAL_POD_AS_BLOB_N(varialble, val_name) \
-  static_assert(std::is_pod<decltype(this_ref.varialble)>::value, "t_type must be a POD type."); \
-  KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(varialble, val_name)
+#define KV_SERIALIZE_VAL_POD_AS_BLOB_N(variable, val_name) \
+  static_assert(std::is_pod<decltype(this_ref.variable)>::value, "t_type must be a POD type."); \
+  KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(variable, val_name)
 
-#define KV_SERIALIZE_VAL_POD_AS_BLOB_OPT_N(varialble, val_name, default_value) \
+#define KV_SERIALIZE_VAL_POD_AS_BLOB_OPT_N(variable, val_name, default_value) \
   do { \
-    static_assert(std::is_pod<decltype(this_ref.varialble)>::value, "t_type must be a POD type."); \
-    bool ret = KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(varialble, val_name); \
+    static_assert(std::is_pod<decltype(this_ref.variable)>::value, "t_type must be a POD type."); \
+    bool ret = KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(variable, val_name); \
     if (!ret) \
-      epee::serialize_default(this_ref.varialble, default_value); \
+      epee::serialize_default(this_ref.variable, default_value); \
   } while(0);
 
-#define KV_SERIALIZE_CONTAINER_POD_AS_BLOB_N(varialble, val_name) \
-  epee::serialization::selector<is_store>::serialize_stl_container_pod_val_as_blob(this_ref.varialble, stg, hparent_section, val_name);
+#define KV_SERIALIZE_CONTAINER_POD_AS_BLOB_N(variable, val_name) \
+  epee::serialization::selector<is_store>::serialize_stl_container_pod_val_as_blob(this_ref.variable, stg, hparent_section, val_name);
 
 #define END_KV_SERIALIZE_MAP() return true;}
 
-#define KV_SERIALIZE(varialble)                           KV_SERIALIZE_N(varialble, #varialble)
-#define KV_SERIALIZE_VAL_POD_AS_BLOB(varialble)           KV_SERIALIZE_VAL_POD_AS_BLOB_N(varialble, #varialble)
-#define KV_SERIALIZE_VAL_POD_AS_BLOB_OPT(varialble, def)  KV_SERIALIZE_VAL_POD_AS_BLOB_OPT_N(varialble, #varialble, def)
-#define KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE(varialble)     KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(varialble, #varialble) //skip is_pod compile time check
-#define KV_SERIALIZE_CONTAINER_POD_AS_BLOB(varialble)     KV_SERIALIZE_CONTAINER_POD_AS_BLOB_N(varialble, #varialble)
+#define KV_SERIALIZE(variable)                           KV_SERIALIZE_N(variable, #variable)
+#define KV_SERIALIZE_VAL_POD_AS_BLOB(variable)           KV_SERIALIZE_VAL_POD_AS_BLOB_N(variable, #variable)
+#define KV_SERIALIZE_VAL_POD_AS_BLOB_OPT(variable, def)  KV_SERIALIZE_VAL_POD_AS_BLOB_OPT_N(variable, #variable, def)
+#define KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE(variable)     KV_SERIALIZE_VAL_POD_AS_BLOB_FORCE_N(variable, #variable) //skip is_pod compile time check
+#define KV_SERIALIZE_CONTAINER_POD_AS_BLOB(variable)     KV_SERIALIZE_CONTAINER_POD_AS_BLOB_N(variable, #variable)
 #define KV_SERIALIZE_OPT(variable,default_value)          KV_SERIALIZE_OPT_N(variable, #variable, default_value)
 /// same as KV_SERIALIZE_OPT, but will set `explicitly_set` to true if non-default value found
 #define KV_SERIALIZE_OPT2(variable,default_value)          KV_SERIALIZE_OPT_N2(variable, #variable, default_value)
