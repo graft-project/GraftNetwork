b141f3e2fbfd1e760e09b2793d763b601eb8b351
Ilya Kitaev <mbg033@gmail.com>
2022-03-28 21:52:12 +0300

    fix: messed up max_version/min_version

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1422,11 +1422,11 @@ bool Blockchain::validate_miner_transaction(const block& b, size_t cumulative_bl
       return false;
     }
 
-    txversion min_version = transaction::get_max_version_for_hf(version);
-    txversion max_version = transaction::get_min_version_for_hf(version);
+    txversion max_version = transaction::get_max_version_for_hf(version);
+    txversion min_version = transaction::get_min_version_for_hf(version);
     if (b.miner_tx.version < min_version || b.miner_tx.version > max_version)
     {
-      MERROR_VER("Coinbase invalid version: " << b.miner_tx.version << " for hardfork: " << version << " min/max version:  " << min_version << "/" << max_version);
+      MERROR_VER("Coinbase invalid version: " << b.miner_tx.version << " for hardfork: " << (int)version << " min/max version:  " << min_version << "/" << max_version);
       return false;
     }
   }
